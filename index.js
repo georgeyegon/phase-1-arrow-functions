d07e72f3377b5cda52125abc22b13b09685b1027
George Yegon <georgeyegon67@gmail.com>
2024-09-24 15:45:16 +0300

    Added arrow functions

---
+++ index.js
@@ -0,0 +1,15 @@
+// Function for divide
+const divide = function(a, b) {
+    return a / b;
+  };
+  
+  // Function for square
+  const square = x => x * x;
+  
+  // Function for add
+  const add = (a, b) => a + b;
+  
+  console.log(divide(2000, 100)); 
+  console.log(square(5)); 
+  console.log(add(3, 7)); 
+  
